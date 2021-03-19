731d3565acbf9ef47fd7379def8e6ef822b0474d
unknown <rajeevupadhyay608@gmail.com>
2021-03-19 15:59:03 +0530

    request movie

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,32 +1,35 @@
 import React from 'react'
 import { Button } from '@material-ui/core/';
 import axios from 'axios'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+  
 function Contact() {
 
-    const [Name, setName] = React.useState("")
 
+    const [Name, setName] = React.useState("")
+    const notify = () => toast("We added soon this request");
     const requestMovies=()=>{
         console.log("HELLO WORLD")
-        fetch("https://myappsmovie.herokuapp.com/request/movie",{
-            method:'post',
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({name:Name})
-        }).then((res)=>res.json()).then((data)=>{console.log(data)});
+
+        var url=`https://api.telegram.org/bot1734489529:AAGHn65vPERAAmSnZMsLYcRxtCyK8dGwMq8/sendMessage?chat_id=-1001328492675&text=Admin please Add this movie  ${Name}`
+        axios.get(url).then((data)=>{
+            setName('')
+            notify()
+        })
      
     }
     return (
         <>
             <div className="request-form" style={{marginTop:"100px",width:"100%",textAlign:"center"}}>
-                    <h3>
+                    <h4>
                         Have a TV show or movie you'd like to see on EntertainmentFlix? Let us know about it below.
                         <br></br>
                         After your request we will add within 5 minutes
                         
-                    </h3>
+                    </h4>
                     <br></br>
+                    <ToastContainer />
                     <h1>Try Now</h1>
                     <input type="text" value={Name} onChange={(e)=>{
                         
